bd5c25d434769089626a072cd700957494dc1bc7
smkrizan <smkrizan@gmail.com>
2020-09-24 15:24:05 -0500

    initial commit

---
+++ assets/js/questions.js
@@ -0,0 +1,63 @@
+// CODE QUIZ: 10 true and false questions and their answers
+var questions = [
+    {
+        question: 'The closest() method searches for a match within parent relationships whereas the querySelector() method searches for a match within descendent relationships.',
+        options: ['T', 'F'], 
+        answer: 'T',
+        ifWrong: "INCORRECT: the statement above is true."
+    },
+    {
+        question: 'DOM stands for Document Object Model', 
+        options: ['T', 'F'],
+        answer: 'T',
+        ifWrong: "INCORRECT: the statement above is true."
+    },
+    {
+        question: 'API stands for Application Parameter Interface', 
+        options: ['T', 'F'],
+        answer: 'F',
+        ifWrong: "INCORRECT: API stands for Application Programming Interface"
+    },
+    {
+        question: 'answer callback function is passed as an argument to another function', 
+        options: ['T', 'F'],
+        answer: 'T',
+        ifWrong: "INCORRECT: the statement above is true."
+    },
+    {
+        question: 'JavaScript data types include numbers, strings, objects, boolean, function, and array', 
+        options: ['T', 'F'],
+        answer: 'T',
+        ifWrong: "INCORRECT: the statement above is true."
+    },
+    {
+        question: 'Parameters are the actual value/s that get/s passed through the parameter into the function to be used', 
+        options: ['T', 'F'],
+        answer: 'F',
+        ifWrong: "INCORRECT: Parameters are placeholder variables where their values are defined when the function is called and are not assigned values within the function itself'"
+    },
+    {
+        question: 'Arguments are placeholder variables where their values are defined when the function is called and are not assigned values within the function itself', 
+        options: ['T', 'F'],
+        answer: 'F',
+        ifWrong: "INCORRECT: Arguments are are the actual value/s that get/s passed through the parameter into the function to be used."
+    },
+    {
+        question: 'JSON stands for JavaScript Object Notification', 
+        options: ['T', 'F'],
+        answer: 'F',
+        ifWrong: "INCORRECT: JSON stands for Javascript Object Notation"
+    },
+    {
+        question: 'Two browser specific functions that can be used to gather user input are <prompt> and <confirm>', 
+        options: ['T', 'F'],
+        answer: 'T',
+        ifWrong: "INCORRECT: the statement above is true."
+    },
+    {
+        question: 'It is a best practice to assign reserved words as variables.', 
+        options: ['T', 'F'],
+        answer: 'F',
+        ifWrong: "INCORRECT: reserved words like 'default' and 'export' should not be used as variables."
+    },
+]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,64 @@
-//two main ways of timing things in javascript
-//setInterval takes two arguments, a function and a number and runs the function every x milliseconds 
-var myInterval = setInterval(function () {
-    console.log("I happened!")
-}, 2 * 1000)
+//GLOBAL VARIABLES
+var time = 60;
+var score = 0;
+
+//DOM ELEMENTS
+var timerEl = document.getElementById("timer");
+var questionsEl = document.getElementById("questions");
+var startBtn = document.getElementById("start-quiz");
+var trueBtn = document.getElementById("true");
+var falseBtn = document.getElementById("false");
+var scoreBtn = document.getElementById("score");
+var qSpaceEl = document.getElementById("question-space");
+var aSpaceEl = document.getElementById("answer-space");
+var initialsEl = document.getElementById("initials");
+
+// START BUTTON will trigger presentation of the first question in place of the instructions within the "question-space"
+//hide quiz instructions
+
+//start countdown
+startButton.onClick = countdown;
+//get question
+
+// TIMER function counts down from 60s once 'start quiz' button is clicked, with a 5s penalty for wrong answers.
 //setTimeout takes two arguments, a function and a number and runs the function once after x milliseconds have passed
-var myTimeOut = setTimeout(function () {
-    console.log("so did I!")
-}, 5 * 1000)
-//because we assigned each of our intervals to a variable, we can cancel them using clearInterval
-function stopIntervals () {
-    clearInterval(myInterval);
-    clearInterval(myTimeOut)
+var startTimer = setTimeout(stopTimer(), 60 * 1000);
+//the timer is stopped with 'clearInterval'
+var stopTimer = function () {
+   if (timer < 0) {
+       clearInterval(timer);
+   }
 }
-//Just like with event listeners, we can use an anonymous function like in the previous examples, or
-//pass a function defined elsewhere. Note the lack of parens after the function name
-var myTimeOut = setInterval(stopIntervals, 10 * 1000)
+
+//SCORING FUNCTION will award 2 points per question. At the end of the game, any time remaing will be added to the score, so 10 seconds remaining=10 points.
+// We start the game with a score of 0.
+
+
+// ANSWER SUBMIT function will display whether answer is correct and move to new question
+// Loop over each object within the 'questions' array
+for (var i = 0; i < questions.length; i++) {
+    // Display current question to user and T/F buttons
+    // User clicks either 'T' button or 'F' button
+    var userAnswer = (questions[i].answer);
+
+    // Compare answers
+    if (
+      (userAnswer === T && questions[i].answer === 'T') ||
+      (userAnswer === F && questions[i].answer === 'F');
+    ) {
+      // Increase score
+      score = score + 2;
+      //display "That is correct!";
+    } else {
+      // display message if incorrect answer is provided and subtract 5 seconds from clock timer
+      questions[i].ifWrong
+      timer = timer - 5;
+    }
+  }
+// SAVE STATS function takes and saves input of the user's initials and stores them along with the user's score  
+
+// QUIZ END function is triggered either by completion of all questions or reaching the maximum time available for the quiz (60 seconds)
+// IF TIME RUNS OUT before the quiz is complete, a "You're out of time - try again!" message displays and the "start quiz" button re-appears
+// IF USER COMPLETES QUIZ, a "Congratulations! You finished in time" message displays and the final score is displayed with input area for user's initials
+
+
