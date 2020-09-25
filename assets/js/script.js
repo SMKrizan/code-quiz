6a56e2ff59030c67e37eeba5f8daa308a43f5f65
smkrizan <smkrizan@gmail.com>
2020-09-25 14:41:37 -0500

    NEXT button functionality issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,52 +10,35 @@ var falseBtn = document.querySelector("button.false");
 var questionSpaceEl = document.querySelector("p.question-space");
 var answerSpaceEl = document.querySelector("p.answer-space");
 var timerEl = document.querySelector("span");
-// var startBtn = document.getElementsByClassName("start-quiz");
+var startBtn = document.getElementById("start-quiz")
 var scoreBtn = document.getElementById("score");
 var initialsEl = document.getElementById("initials");
 
 // set up for delegating click responsibility to div.container containing button elements
 var pageContentEl = document.querySelector("container");
 
-// this function listens to events within entire div.container document (similar to Taskinator) 
-var quizButtonHandler = function (event) {
-    // 'event.target' reports the element on which the event ("click") occurs
-    console.log(event.target);
+// QUIZ STARTS once 'start quiz' button is clicked
+var startQuiz = function (event) {
+    // prevents browser from reloading
+    event.preventDefault();
 
-    // get target element from event
-    var targetEl = event.target;
+    console.log("Starting the quiz!");
+    startTimer()
+    // replaces quiz instructions with question
+    questionSpaceEl.textContent = questions[currentQ].question;
+    // updating CSS property to reveal 'T' and 'F' buttons
+    trueBtn.removeAttribute("id");
+    falseBtn.removeAttribute("id");
+}
 
-    // START BUTTON was clicked
-    if (targetEl.matches(".start-quiz")) {
-        // start timer countdown
-        startTimer()
-        // present questions
-        selectAnswer()
-        // unhide TRUE/FALSE buttons
-        unhideTrueBtn = trueBtn.removeAttribute("id");
-        unhideFalseBtn = falseBtn.removeAttribute("id");
-    }
+// VIEW HIGH SCORES BUTTON was clicked; 
+var displayHighScores = function (event) {
+    event.preventDefault();
 
-    // TRUE BUTTON was clicked; 
-    else if (targetEl.matches(".true")) {
-        userSelection = "T";
-        currentQ++;
-        selectAnswer();
-    }
-
-    // FALSE BUTTON was clicked;
-    else if (targetEl.matches(".false")) {
-        userSelection = "F";
-        currentQ++;
-        selectAnswer();
-    }
-
-    // VIEW HIGH SCORES BUTTON was clicked; 
-    else if (targetEl.matches(".scores")) {
-        document.getElementById("index").style.display = "none";
-        document.getElementById("high-scores").style.display = "block";
-    }
-};
+    (targetEl.matches(".scores"))
+    document.getElementById("index").style.display = "none";
+    document.getElementById("high-scores").style.display = "block";
+}
 
 //START TIMER function counts down from 60s once 'start quiz' button is clicked
 var startTimer = function () {
@@ -64,35 +47,67 @@ var startTimer = function () {
     //the timer is stopped with 'clearInterval'
     if (time = 0) {
         clearInterval(startTimer);
-        quizOver();
+        quizOver()
     }
 }
 
-// SELECT ANSWER function will wait for T/F selection, display whether answer is correct and move to new question
-var selectAnswer = function () {
-    if (currentQ < 10) {
-        // Display current question and T/F buttons
+// DISPLAY QUESTION function will present the current question
+var displayQuestion = function () {
+    if (currentQ < questions.length) {
+        // Display current question
         questionSpaceEl.textContent = questions[currentQ].question;
-        // Compare answers
-        if ((userSelection === 'T' && questions[currentQ].answer === 'T') ||
-            (userSelection === 'F' && questions[currentQ].answer === 'F')) {
-            // Increase score
-            score = score + 2;
-            console.log(score);
-            // affirm selection;
-            answerSpaceEl.textContent = "That is Correct!";
-        }
-        else if {
-            // display message re incorrect answer and subtract 5 seconds from clock timer
-            ((document.trueBtn.addEventListener("click", selectAnswer)) || 
-            (document.trueBtn.addEventListener("click", selectAnswer)))
-            answerSpaceEl.textContent = questions[currentQ].ifWrong;
-            time = time - 5;
-        }
     }
     else {
         quizOver()
     }
+}
+
+// determine whether user selects 'true' or 'false'
+var trueOrFalse = function (event) {
+    event.preventDefault();
+    // 'event.target' reports the element on which the event ("click") occurs
+    console.log(event.target);
+    // get target element from event
+    var targetEl = event.target;
+    // TRUE BUTTON was clicked; 
+    if (targetEl.matches(".true")) {
+        userSelection = "T";
+    }
+    // FALSE BUTTON was clicked;
+    else if (targetEl.matches(".false")) {
+        userSelection = "F";
+    }
+    assessUserSelection()
+}
+
+// let user know whether their selection is correct and move to next question
+var assessUserSelection = function () {
+    if
+        ((userSelection === 'T' && questions[currentQ].answer === 'T') ||
+        (userSelection === 'F' && questions[currentQ].answer === 'F')) {
+        // Increase score
+        score = score + 2;
+        console.log(score);
+        // affirm selection;
+        answerSpaceEl.textContent = "That is Correct!";
+    }
+    else {
+        // display message re incorrect answer and subtract 5 seconds from clock timer
+        answerSpaceEl.textContent = questions[currentQ].ifWrong;
+    }
+    interimState()
+}
+
+var interimState = function() {
+    trueBtn.setAttribute("id", "hide");
+    falseBtn.setAttribute("id", "hide");
+    startBtn.textContent = "NEXT";
+    nextQuestion()
+}
+
+var nextQuestion = function() {
+    currentQ ++;
+    displayQuestion()
 }
 
 // QUIZ OVER function is triggered either by completion of all questions or reaching the maximum time available for the quiz (60 seconds)
@@ -104,9 +119,12 @@ quizOver = function () {
     else {
         score = score + time;
         answerSpaceEl.textContent = "Quiz complete! Your score is " + score + ". Click the 'View High Scores' button to save your stats.";
-    } 
+    }
 }
 
 // SAVE STATS function takes and saves input of the user's initials and stores them along with the user's score 
 
-pageContentEl = addEventListener("click", quizButtonHandler)
+//pageContentEl.addEventListener("click", quizButtonHandler)
+startBtn.addEventListener("click", startQuiz)
+trueBtn.addEventListener("click", trueOrFalse)
+falseBtn.addEventListener("click", trueOrFalse)
