1302eb7bbe283427a57df44dc2b4c262d9bd5e9e
smkrizan <smkrizan@gmail.com>
2020-09-25 12:29:04 -0500

    selectAnswer issue

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -7,49 +7,49 @@ var questions = [
         ifWrong: "INCORRECT: the statement above is true."
     },
     {
-        question: 'DOM stands for Document Object Model', 
+        question: 'DOM stands for Document Object Model.', 
         options: ['T', 'F'],
         answer: 'T',
         ifWrong: "INCORRECT: the statement above is true."
     },
     {
-        question: 'API stands for Application Parameter Interface', 
+        question: 'API stands for Application Parameter Interface.', 
         options: ['T', 'F'],
         answer: 'F',
         ifWrong: "INCORRECT: API stands for Application Programming Interface"
     },
     {
-        question: 'answer callback function is passed as an argument to another function', 
+        question: 'A callback function is a function that is passed as an argument to another function.', 
         options: ['T', 'F'],
         answer: 'T',
         ifWrong: "INCORRECT: the statement above is true."
     },
     {
-        question: 'JavaScript data types include numbers, strings, objects, boolean, function, and array', 
+        question: 'JavaScript data types include numbers, strings, objects, boolean operators, functions, and arrays.', 
         options: ['T', 'F'],
         answer: 'T',
         ifWrong: "INCORRECT: the statement above is true."
     },
     {
-        question: 'Parameters are the actual value/s that get/s passed through the parameter into the function to be used', 
+        question: 'Parameters are the actual value/s that get/s passed through the parameter into the function to be used.', 
         options: ['T', 'F'],
         answer: 'F',
         ifWrong: "INCORRECT: Parameters are placeholder variables where their values are defined when the function is called and are not assigned values within the function itself'"
     },
     {
-        question: 'Arguments are placeholder variables where their values are defined when the function is called and are not assigned values within the function itself', 
+        question: 'Arguments are placeholder variables where their values are defined when the function is called and are not assigned values within the function itself.', 
         options: ['T', 'F'],
         answer: 'F',
         ifWrong: "INCORRECT: Arguments are are the actual value/s that get/s passed through the parameter into the function to be used."
     },
     {
-        question: 'JSON stands for JavaScript Object Notification', 
+        question: 'JSON stands for JavaScript Object Notification.', 
         options: ['T', 'F'],
         answer: 'F',
         ifWrong: "INCORRECT: JSON stands for Javascript Object Notation"
     },
     {
-        question: 'Two browser specific functions that can be used to gather user input are <prompt> and <confirm>', 
+        question: 'Two browser specific functions that can be used to gather user input are <prompt> and <confirm>.', 
         options: ['T', 'F'],
         answer: 'T',
         ifWrong: "INCORRECT: the statement above is true."
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,93 +3,32 @@ var time = 60;
 var score = 0;
 currentQ = 0;
 userSelection = "";
-// var questions = [
-//     {
-//         question: 'The closest() method searches for a match within parent relationships whereas the querySelector() method searches for a match within descendent relationships.',
-//         options: ['T', 'F'],
-//         answer: 'T',
-//         ifWrong: "INCORRECT: the statement above is true."
-//     },
-//     {
-//         question: 'DOM stands for Document Object Model',
-//         options: ['T', 'F'],
-//         answer: 'T',
-//         ifWrong: "INCORRECT: the statement above is true."
-//     },
-//     {
-//         question: 'API stands for Application Parameter Interface',
-//         options: ['T', 'F'],
-//         answer: 'F',
-//         ifWrong: "INCORRECT: API stands for Application Programming Interface"
-//     },
-//     {
-//         question: 'answer callback function is passed as an argument to another function',
-//         options: ['T', 'F'],
-//         answer: 'T',
-//         ifWrong: "INCORRECT: the statement above is true."
-//     },
-//     {
-//         question: 'JavaScript data types include numbers, strings, objects, boolean, function, and array',
-//         options: ['T', 'F'],
-//         answer: 'T',
-//         ifWrong: "INCORRECT: the statement above is true."
-//     },
-//     {
-//         question: 'Parameters are the actual value/s that get/s passed through the parameter into the function to be used',
-//         options: ['T', 'F'],
-//         answer: 'F',
-//         ifWrong: "INCORRECT: Parameters are placeholder variables where their values are defined when the function is called and are not assigned values within the function itself'"
-//     },
-//     {
-//         question: 'Arguments are placeholder variables where their values are defined when the function is called and are not assigned values within the function itself',
-//         options: ['T', 'F'],
-//         answer: 'F',
-//         ifWrong: "INCORRECT: Arguments are are the actual value/s that get/s passed through the parameter into the function to be used."
-//     },
-//     {
-//         question: 'JSON stands for JavaScript Object Notification',
-//         options: ['T', 'F'],
-//         answer: 'F',
-//         ifWrong: "INCORRECT: JSON stands for Javascript Object Notation"
-//     },
-//     {
-//         question: 'Two browser specific functions that can be used to gather user input are <prompt> and <confirm>',
-//         options: ['T', 'F'],
-//         answer: 'T',
-//         ifWrong: "INCORRECT: the statement above is true."
-//     },
-//     {
-//         question: 'It is a best practice to assign reserved words as variables.',
-//         options: ['T', 'F'],
-//         answer: 'F',
-//         ifWrong: "INCORRECT: reserved words like 'default' and 'export' should not be used as variables."
-//     },
-// ];
 
 //DOM ELEMENTS
 var trueBtn = document.querySelector("button.true");
 var falseBtn = document.querySelector("button.false");
 var questionSpaceEl = document.querySelector("p.question-space");
 var answerSpaceEl = document.querySelector("p.answer-space");
-var timerEl = document.getElementById("timer");
-var startBtn = document.getElementsByClassName("start-quiz");
+var timerEl = document.querySelector("span");
+// var startBtn = document.getElementsByClassName("start-quiz");
 var scoreBtn = document.getElementById("score");
 var initialsEl = document.getElementById("initials");
 
 // set up for delegating click responsibility to div.container containing button elements
 var pageContentEl = document.querySelector("container");
 
-//(similar to taskinator) this function listens to events within entire div.container document
+// this function listens to events within entire div.container document (similar to Taskinator) 
 var quizButtonHandler = function (event) {
     // 'event.target' reports the element on which the event ("click") occurs
     console.log(event.target);
 
     // get target element from event
     var targetEl = event.target;
+
     // START BUTTON was clicked
     if (targetEl.matches(".start-quiz")) {
         // start timer countdown
-
+        startTimer()
         // present questions
         selectAnswer()
         // unhide TRUE/FALSE buttons
@@ -100,42 +39,36 @@ var quizButtonHandler = function (event) {
     // TRUE BUTTON was clicked; 
     else if (targetEl.matches(".true")) {
         userSelection = "T";
-        currentQ ++;
+        currentQ++;
         selectAnswer();
     }
 
     // FALSE BUTTON was clicked;
     else if (targetEl.matches(".false")) {
         userSelection = "F";
-        currentQ ++;
+        currentQ++;
         selectAnswer();
     }
 
     // VIEW HIGH SCORES BUTTON was clicked; 
     else if (targetEl.matches(".scores")) {
-
+        document.getElementById("index").style.display = "none";
+        document.getElementById("high-scores").style.display = "block";
     }
 };
-// START BUTTON will trigger presentation of the first question in place of the instructions within the "question-space"
 
-//start countdown
-// startBtn.onClick = countdown;
-//get question
-
-//TIMER function counts down from 60s once 'start quiz' button is clicked, with a 5s penalty for wrong answers.
-//setTimeout takes two arguments, a function and a number and runs the function once after x milliseconds have passed
-var startTimer = setInterval(stopTimer(1000));
-//the timer is stopped with 'clearInterval'
-var stopTimer = function () {
-   if (time = 0) {
-   }
+//START TIMER function counts down from 60s once 'start quiz' button is clicked
+var startTimer = function () {
+    var countdown = time - 1;
+    timerEl.textContent = setInterval(countdown, 1000);
+    //the timer is stopped with 'clearInterval'
+    if (time = 0) {
+        clearInterval(startTimer);
+        quizOver();
+    }
 }
 
-//SCORING FUNCTION will award 2 points per question. At the end of the game, any time remaing will be added to the score, so 10 seconds remaining=10 points.
-// We start the game with a score of 0.
-
-
-// TRUE/FALSE BUTTONS function will display whether answer is correct and move to new question
+// SELECT ANSWER function will wait for T/F selection, display whether answer is correct and move to new question
 var selectAnswer = function () {
     if (currentQ < 10) {
         // Display current question and T/F buttons
@@ -146,25 +79,34 @@ var selectAnswer = function () {
             // Increase score
             score = score + 2;
             console.log(score);
-            // Affirm selection;
+            // affirm selection;
             answerSpaceEl.textContent = "That is Correct!";
-            // Move to next question
-        } 
-        else {
-            // display message if incorrect answer is provided and subtract 5 seconds from clock timer
-            questions[currentQ].ifWrong;
-            // timer = timer - 5;
+        }
+        else if {
+            // display message re incorrect answer and subtract 5 seconds from clock timer
+            ((document.trueBtn.addEventListener("click", selectAnswer)) || 
+            (document.trueBtn.addEventListener("click", selectAnswer)))
+            answerSpaceEl.textContent = questions[currentQ].ifWrong;
+            time = time - 5;
         }
     }
     else {
-        document.getElementById("index").style.display = "none";
-        document.getElementById("high-scores").style.display = "block";
-    }  
+        quizOver()
+    }
 }
-    // SAVE STATS function takes and saves input of the user's initials and stores them along with the user's score  
 
-    // QUIZ END function is triggered either by completion of all questions or reaching the maximum time available for the quiz (60 seconds)
-    // IF TIME RUNS OUT before the quiz is complete, a "You're out of time - try again!" message displays and the "start quiz" button re-appears
-    // IF USER COMPLETES QUIZ, a "Congratulations! You finished in time" message displays and the final score is displayed with input area for user's initials
+// QUIZ OVER function is triggered either by completion of all questions or reaching the maximum time available for the quiz (60 seconds)
+quizOver = function () {
+    if (time === 0 && currentQ < 10) {
+        answerSpaceEl.textContent = "You have run out of time. Your score is " + score + ". Try again!";
+    }
+    // a message displays with final score and option to input user initials
+    else {
+        score = score + time;
+        answerSpaceEl.textContent = "Quiz complete! Your score is " + score + ". Click the 'View High Scores' button to save your stats.";
+    } 
+}
 
-    pageContentEl = addEventListener("click", quizButtonHandler)
+// SAVE STATS function takes and saves input of the user's initials and stores them along with the user's score 
+
+pageContentEl = addEventListener("click", quizButtonHandler)
