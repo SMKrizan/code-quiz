f51d10dd2bcf76da50ebd90b59bfb16cbcc4e694
smkrizan <smkrizan@gmail.com>
2020-09-26 09:20:45 -0500

    clean-up and form prep

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -5,6 +5,25 @@ returnBtn = document.querySelector("button");
 // var highscores =
 //     JSON.parse(window.localStorage.getItem("highscores")) || [];
 
+// // LOAD STATS function to get tasks from localStorage, convert from string format back into an array of objects and iterates through tasks array to create the elements on the page
+    // var loadStats = function () {
+    //     // get tasks from localStorage and account for the condition where there are no saved tasks
+    //     var savedStats = localStorage.getItem("stats");
+
+    //     if (savedStats === null) {
+    //         return false;
+    //     }
+    //     // convert from string to an array of objects
+    //     savedStats = JSON.parse(savedStats);
+
+    //     // iterate through stats array to create elements on the page
+    //     for (var i = 0; i < savedStats.length; i++) {
+    //     // pass each stat object into the 'createStatEl()' function
+    //     createStatEl(savedStats[i]);
+    //     }
+    // }
+
+
 var returnToQuiz = function (event) {
     event.preventDefault();
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,8 @@
 var time = questions.length * 6;
 var score = 0;
 var timerId;
-
-// Establishing question count
+var userStats = [];
 currentQ = 0;
-
-// establishing user's selection input 
 userSelection = "";
 
 //DOM ELEMENTS
@@ -14,47 +11,40 @@ var startBtn = document.getElementById("start-quiz");
 var trueBtn = document.querySelector("button.true");
 var falseBtn = document.querySelector("button.false");
 var nextBtn = document.querySelector("button.next");
+var submitBtn = document.querySelector("div.btn");
 var highScoreBtn = document.getElementById("scores");
 var questionSpaceEl = document.querySelector("p.question-space");
 var answerSpaceEl = document.querySelector("p.answer-space");
 var timerEl = document.getElementById("time");
 var initialsEl = document.getElementById("initials");
-var userInput = document.querySelector("form");
-var quizPage = document.getElementById("wrapper");
-var scoresPage = document.getElementById("high-scores");
+var inputFormEl = document.querySelector("div.form-group");
+console.log(inputFormEl);
 
-// set up for delegating click responsibility to div.container containing button elements
-var pageContentEl = document.querySelector("container");
+
+// var quizPage = document.getElementById("wrapper");
+// var scoresPage = document.getElementById("high-scores");
 
 // QUIZ STARTS once 'start quiz' button is clicked
 var startQuiz = function (event) {
     // prevents browser from reloading
     event.preventDefault();
 
-    // start condition
+    // starting conditions
     time = questions.length * 6;
     score = 0;
     timerId;
     currentQ = 0;
     answerSpaceEl.textContent = "";
-    //removeT&F
     trueBtn.removeAttribute("id");
     falseBtn.removeAttribute("id");
-    //remove form & start button
-    userInput.setAttribute("id", "hide");
+    inputFormEl.setAttribute("id", "hide");
     startBtn.setAttribute("id", "hide");
-
-    // establishing user's selection input 
     userSelection = "";
 
     timerEl.textContent = time;
 
     // replaces quiz instructions with question
     questionSpaceEl.textContent = questions[currentQ].question;
-    // // updating CSS property to reveal 'T' and 'F' buttons
-    // trueBtn.removeAttribute("id");
-    // falseBtn.removeAttribute("id");
-    // startBtn.setAttribute("id", "hide");
 
     console.log("Starting the quiz!");
     startTimer()
@@ -127,6 +117,7 @@ var wrongAnswer = function () {
     interimState()
 }
 
+// TRANSITION state settings
 var interimState = function () {
     trueBtn.setAttribute("id", "hide");
     falseBtn.setAttribute("id", "hide");
@@ -134,6 +125,7 @@ var interimState = function () {
     nextBtn.addEventListener("click", nextQuestion);
 }
 
+// QUESTION state settings
 var nextQuestion = function () {
     nextBtn.setAttribute("id", "hide");
     trueBtn.removeAttribute("id");
@@ -143,23 +135,22 @@ var nextQuestion = function () {
     displayQuestion()
 }
 
-// QUIZ OVER function is triggered either by completion of all questions or reaching the maximum time available for the quiz (60 seconds)
+// QUIZ OVER function, triggered either by completion of all questions or running out of time
 quizOver = function () {
     clearInterval(timerId);
     trueBtn.setAttribute("id", "hide");
     falseBtn.setAttribute("id", "hide");
-    userInput.removeAttribute("id");
+    inputFormEl.removeAttribute("id");
     startBtn.removeAttribute("id");
+    submitBtn.removeAttribute("id", "hide")
     questionSpaceEl.textContent = "";
-    
-    var message = "";
+
     // a message displays with final score and option to input user initials
+    var message = "";
     if (time === 0 && currentQ < 10) {
         message += "You have run out of time. "
-    }
-    else {
+    } else {
         score = score + time;
-        // answerSpaceEl.textContent = "Quiz complete! "
         message += "Quiz complete! "
     }
     answerSpaceEl.textContent = message + "Your score is " + score + ". Enter your name or initials and click 'submit' or play again!";
@@ -171,64 +162,58 @@ quizOver = function () {
 var countdown = function () {
     time = time - 1;
     timerEl.textContent = time;
-
     if (time <= 0) {
         quizOver();
     }
 }
 
+//FORM HANDLER - function for managing user input to form
+var formHandler = function (event) {
+    // var nameInput = document.querySelector("input[name = 'user-name']").nodeValue;
+    // var score = "";
+
+    // // make sure value wasn't empty
+    // if (inputForm !== "") {
+}
+
 // SAVE STATS function collects user input (initials) and stores them in object with user's score
 function saveStats() {
 
-    highScoreBtn.innerHTML = "SUBMIT";
     // user stats object
+    var userStats = [
+        {
+            userInput: userName,
+            score: userScore,
+            id: ''
+        }
+    ]
 
-    // make sure value wasn't empty
-    if (userInput !== "") {
-
-        // save to localstorage
-        userInput.push(newScore);
-        window.localStorage.setItem("highscores", JSON.stringify(highscores));
-    }
+    // save to localstorage
+    userInput.push(newScore);
+    window.localStorage.setItem("highscores", JSON.stringify(highscores));
 }
 
-    // VIEW HIGH SCORES BUTTON was clicked; 
-    var displayHighScores = function (event) {
-        event.preventDefault();
+// VIEW HIGH SCORES BUTTON was clicked; 
+var displayHighScores = function (event) {
+    event.preventDefault();
 
-        location.href = "high-scores.html";
+    location.href = "high-scores.html";
 
-    }
+}
 
-    var saveInitials = function () {
-        //reveal start button & user input form
-        startBtn.removeAttribute("id");
-        userInput.removeAttribute("id");
+var saveStats = function () {
+    //reveal start button & user input form
+    startBtn.removeAttribute("id");
+    inputFormEl.removeAttribute("id");
+    submitBtn.removeAttribute("id", "hide")
 
-        localStorage.setItem("initials", initialsEl);
-    }
-
-    // // LOAD STATS function to get tasks from localStorage, convert from string format back into an array of objects and iterates through tasks array to create the elements on the page
-    // var loadStats = function () {
-    //     // get tasks from localStorage and account for the condition where there are no saved tasks
-    //     var savedStats = localStorage.getItem("stats");
-
-    //     if (savedStats === null) {
-    //         return false;
-    //     }
-    //     // convert from string to an array of objects
-    //     savedStats = JSON.parse(savedStats);
-
-    //     // iterate through stats array to create elements on the page
-    //     for (var i = 0; i < savedStats.length; i++) {
-    //     // pass each stat object into the 'createStatEl()' function
-    //     createStatEl(savedStats[i]);
-    //     }
-    // }
+    localStorage.setItem("initials", initialsEl);
+}
 
 
-    // EVENT LISTENERS for button click events
-    startBtn.addEventListener("click", startQuiz)
-    trueBtn.addEventListener("click", trueOrFalse)
-    falseBtn.addEventListener("click", trueOrFalse)
-    highScoreBtn.addEventListener("click", displayHighScores)
+// EVENT LISTENERS for button click events
+startBtn.addEventListener("click", startQuiz)
+trueBtn.addEventListener("click", trueOrFalse)
+falseBtn.addEventListener("click", trueOrFalse)
+highScoreBtn.addEventListener("click", displayHighScores)
+inputFormEl.addEventListener("submit", formHandler)
